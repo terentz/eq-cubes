516ecf0b7fdb54cbacf59c182be668026ff72a07
Tristan Rentz <terentz.hexadec@gmail.com>
2017-04-14 03:03:52 +1000

    Falling asleep; saving work!

--- js/cubes.js
+++ js/cubes.js
@@ -1,15 +1,10 @@
 var notesArr =  [ { oct: -1, loHz: 7.9, hiHz: 15.9 },
                   { oct: 0, loHz:}]
 
-eq.freq2noteName = function(freq){
-  var h = Math.round(12*Math.log2(freq/eq.C0));
-  var octave = Math.floor(h/12);
-  var n = h%12
-  return eq.names[n]+octave.toString();
-};
+
 
 eq.note2parts = function(note){
-  
+
 };
 
 
